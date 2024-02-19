5977c66dc9ded0bb8d209483eef2cdcc10461bd8
khangzxrr <khangzxrr@gmail.com>
2024-02-19 21:53:11 +0700

    migrate to react router v6

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -1,17 +1,15 @@
 
-import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import { Home, Login } from  '../pages';
 
-const Routers = () => {
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          {/* <Route path="*" element={<ErrorPage />} /> */}
-        </Routes>
-      </Router>
-    </div>
-}
-
+const Routers = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home />
+    },
+    {
+        path: '/login',
+        element: <Login />
+    }
+])
 export default Routers;
