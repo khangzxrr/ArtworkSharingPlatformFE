dd5789224bd804d11eb69b776aa9c04dbaf4b0d1
khangzxrr <khangzxrr@gmail.com>
2024-02-23 17:35:59 +0700

    add new APIs

---
+++ src/services/requestBidService.jsx
@@ -0,0 +1,29 @@
+import { userAxios } from "../utils/axios"
+
+export const creatorGetRequestBidsOfRequest = async (requestId) =>  {
+    const response = await userAxios.get(`/creator/request/${requestId}/request-bids`)
+
+    return response.data
+}
+
+export const userGetRequestBidsOfRequest = async (requestId) => {
+    const response = await userAxios.get(`/audience/requests/${requestId}/request-bids`)
+
+    return response.data
+}
+
+export const creatorCreateRequestBid = async (requestId, description, price, duration) => {
+    const response = await userAxios.post(`creator/request/${requestId}/request-bids`, {
+        description,
+        price,
+        duration
+    })
+
+    return response.data
+}
+
+export const audienceChooseRequestBid = async (requestId, requestBidId) => {
+    const response = await userAxios.post(`/audience/requests/${requestId}/request-bids/${requestBidId}/choose`) 
+
+    return response.data
+}
