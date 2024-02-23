483344e2ed5bd09143d69f00cd2ce45d38ac5f73
khangzxrr <khangzxrr@gmail.com>
2024-02-23 15:43:42 +0700

    add check role method

--- src/stores/authenticationStore.js
+++ src/stores/authenticationStore.js
@@ -1,5 +1,6 @@
 import { create } from 'zustand'
 import { devtools, persist } from 'zustand/middleware'
+import { CREATOR_AUTHORIZE, USER_AUTHORIZE } from '../utils/constants'
 
 export const useAuthenticationStore = create(devtools(
     persist(
@@ -14,6 +15,9 @@ export const useAuthenticationStore = create(devtools(
   )
 )
 
+export const isContainUserRole = () => useAuthenticationStore.getState().account.authorities.includes(USER_AUTHORIZE)
+
+export const isContainCreatorRole = () => useAuthenticationStore.getState().account.authorities.includes(CREATOR_AUTHORIZE)
 
 export const setToken = (accessToken) => useAuthenticationStore.setState((state) => ({ accessToken }))
 
