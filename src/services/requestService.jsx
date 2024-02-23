33dffc57cff061772ba03c95796ee7119ff9a5a9
khangzxrr <khangzxrr@gmail.com>
2024-02-23 15:27:52 +0700

    remove base url in service

--- src/services/requestService.jsx
+++ src/services/requestService.jsx
@@ -1,22 +1,21 @@
-import axios from "axios"
 import { BASE_URL } from "../utils/constants"
 import { userAxios } from "../utils/axios";
 
 export const userGetRequests = async () => {
 
-    const response = await userAxios.get(BASE_URL + '/audience/requests');
+    const response = await userAxios.get('/audience/requests');
 
     return response.data;
 }
 
 export const userGetRequest = async (requestId) => {
-    const response = await userAxios.get(`${BASE_URL}/audience/requests/${requestId}`)
+    const response = await userAxios.get(`/audience/requests/${requestId}`)
 
     return response.data
 }
 
 export const userCreateRequest = async (title, description) => {
-    const response = await userAxios.post(`${BASE_URL}/audience/requests`, {
+    const response = await userAxios.post(`/audience/requests`, {
         title,
         description,
         attachments: []
