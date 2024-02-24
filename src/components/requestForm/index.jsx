90bcfe0762e6b6e5f6662cfb7ee14c9a9eb92e9a
khangzxrr <khangzxrr@gmail.com>
2024-02-24 23:48:23 +0700

    move API call out of component

--- src/components/requestForm/index.jsx
+++ src/components/requestForm/index.jsx
@@ -1,66 +1,21 @@
 
 
-import React, { useEffect, useState } from 'react';
-import { Divider, Flex, Form, Input, notification } from 'antd';
-import { isContainCreatorRole, isContainUserRole, useAuthenticationStore } from "../../stores/authenticationStore";
-import { creatorGetRequest, userGetRequest } from "../../services/requestService";
-import { useParams } from "react-router-dom";
-import { creatorGetRequestBidsOfRequest, userGetRequestBidsOfRequest } from "../../services/requestBidService";
-import { RequestBidForm, RequestBidList } from "..";
+import React, {  } from 'react';
+import { Divider, Flex, Form, Input } from 'antd';
+import { RequestBidList } from "..";
 
-const Index = () => {
+const Index = (props) => {
 
-  const { requestId } = useParams()
   const [form] = Form.useForm();
 
-  const [request, setRequest] = useState({})
-  const [requestBids, setRequestBids] = useState([])
-
-  const account = useAuthenticationStore(state => state.account)
-
-  function setRequestData(requestData) {
+  if (props.request) {
     form.setFieldsValue({
-      requestTitle: requestData.title,
-      requestDescription: requestData.description
+      requestTitle: props.request.title,
+      requestDescription: props.request.description,
     })
-
-    setRequest(requestData)
   }
+  
 
-  function notifyError(err) {
-    console.log(err)
-    notification.error({ message: 'Get request', description: 'Failed to get request, please try again' })
-  }
-
-  useEffect(() => {
-
-    if (isContainUserRole()) {
-      userGetRequest(requestId)
-        .then(response => {
-          setRequestData(response)
-
-          return userGetRequestBidsOfRequest(requestId)
-        })
-        .then(response => {
-          setRequestBids(response)
-        })
-        .catch(error => notifyError(error))
-
-    } else
-      if (isContainCreatorRole()) {
-        creatorGetRequest(requestId)
-          .then(response => {
-            setRequestData(response)
-
-            return creatorGetRequestBidsOfRequest(requestId)
-          })
-          .then(response => {
-            setRequestBids(response)
-          })
-          .catch(error => notifyError(error))
-      }
-
-  }, [])
 
   return (
     <>
@@ -108,12 +63,12 @@ const Index = () => {
 
         <Divider />
 
-        {(request.user && request.user.login !== account.login && isContainCreatorRole()) &&
+        {/* {(request.user && request.user.login !== account.login && isContainCreatorRole()) &&
           <RequestBidForm requestId={requestId} />
-        }
+        } */}
       </Flex>
 
-      <RequestBidList  request={request} account={account} requestBids={requestBids} />
+      <RequestBidList  request={props.request} requestBids={props.requestBids} />
 
     </>
   );
