9fee2409c1745cf24adc4221add5f9b4645c8f00
khangzxrr <khangzxrr@gmail.com>
2024-02-25 20:31:02 +0700

    remove hardcode, add mapping method, step => card

--- src/components/requestProgressList/index.jsx
+++ src/components/requestProgressList/index.jsx
@@ -1,42 +1,100 @@
 
 
 import React, { } from 'react';
-import { Steps } from 'antd';
+import { Badge, Button, Card, Row, Space, Steps, notification } from 'antd';
+import { Link } from 'react-router-dom';
+import { isContainUserRole } from 'stores/authenticationStore';
+import { userAcceptRequestProgress, userRejectRequestProgress } from 'services/requestProgressService';
 
 
 const Index = (props) => {
 
-  const currentTimeline = props.requestProgresses ? props.requestProgresses.length : 0;
 
-  const timeline = props.requestProgresses.map(rp => {
 
-    if (rp.type === 'FIRST_PAYMENT') {
-      return {
-        title: 'First payment',
-        subTitle: rp.date
-      }
-    }
-    else if (rp.type === 'SECOND_PAYMENT') {
-      return {
-        title: 'Second payment',
-        subTitle: rp.date
-      }
-    }
-    else if (rp.type.contains('REPORT')) {
-      return {
-        title: 'Report',
-        subTitle: rp.date
-      }
-    }
+  const mapStatusToColor = (status) => {
+    if (status === 'PENDING') return 'orange'
+    if (status === 'SUCCEED') return 'green'
 
-  })
+    return 'red'
+  }
+
+
+  const acceptRequestProgress = (requestProgressId) => {
+    userAcceptRequestProgress(props.requestId, requestProgressId)
+      .then(response => notification.info({ message: 'Update request progress', description: 'Accepted request progress report!' }))
+      .catch(error => {
+        console.log(error)
+        notification.error({ message: 'Update request progress', description: 'Fail to accept request progress report!' })
+      })
+      .finally(() => {
+        props.refreshPage()
+      })
+  }
+
+  const rejectRequestProgress = (requestProgressId) => {
+    userRejectRequestProgress(props.requestId, requestProgressId)
+      .then(response => notification.info({ message: 'Update request progress', description: 'Accepted request progress report!' }))
+      .catch(error => {
+        console.log(error)
+        notification.error({ message: 'Update request progress', description: 'Fail to accept request progress report!' })
+      })
+      .finally(() => {
+        props.refreshPage()
+      })
+  }
+
+  const mapReportActions = (requestProgress) => {
+
+    if (requestProgress.status === 'PENDING' && isContainUserRole()) {
+      return <Space direction='horizontal'>
+        <Button type='primary' onClick={() => acceptRequestProgress(requestProgress.id)}>Accept</Button>
+        <Button danger type='primary' onClick={() => rejectRequestProgress(requestProgress.id)}>Reject</Button>
+      </Space>
+    }
+  }
 
   return (
-    <Steps
-      direction='vertical'
-      current={currentTimeline}
-      items={timeline}
-    />
+    <>
+      {
+        props.requestProgresses.map(rp => {
+          if (rp.type === 'FIRST_PAYMENT') {
+            return <Badge.Ribbon text={rp.status} color={mapStatusToColor(rp.status)}>
+              <Card title="First payment" size="small">
+                paid at: {rp.date}
+              </Card>
+            </Badge.Ribbon>
+          }
+          else if (rp.type === 'SECOND_PAYMENT') {
+            return <Badge.Ribbon text={rp.status} color={mapStatusToColor(rp.status)}>
+              <Card title="Second payment" size="small">
+                paid at: {rp.date}
+              </Card>
+            </Badge.Ribbon>
+          }
+          else if (rp.type.includes('REPORT')) {
+
+            return <Badge.Ribbon text={rp.status} color={mapStatusToColor(rp.status)}>
+              <Card
+                title={rp.type}
+                size="small"
+              >
+                <p>created at: {rp.date}</p>
+                <p>description: {rp.description} </p>
+                <Space direction='vertical'>
+                  {
+                    rp.attachments.map(attach => <Link to={attach.media.url}>{attach.media.url}</Link>)
+                  }
+                </Space>
+
+                {mapReportActions(rp)}
+
+              </Card>
+            </Badge.Ribbon>
+          }
+        })
+
+      }
+    </>
   );
 };
 
