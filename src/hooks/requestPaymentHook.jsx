6462ddd02a03a7ea0982277d7bfec2542aab9e81
khangzxrr <khangzxrr@gmail.com>
2024-02-25 15:21:57 +0700

    add amount to workaround error

--- src/hooks/requestPaymentHook.jsx
+++ src/hooks/requestPaymentHook.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import { getFirstPayment, getSecondPayment, payFirstPayment } from "services/requestPaymentService"
 
 export const useGetFirstPayment = (requestId) => {
-    const [firstPayment, setFirstPayment] = useState({})
+    const [firstPayment, setFirstPayment] = useState({ amount: 0 })
 
     useEffect(() => {
         getFirstPayment(requestId).then(response => setFirstPayment(response))
@@ -12,7 +12,7 @@ export const useGetFirstPayment = (requestId) => {
 }
 
 export const useGetSecondPayment = (requestId) => {
-    const [secondPayment, setSecondPayment] = useState({})
+    const [secondPayment, setSecondPayment] = useState({ amount: 0})
 
     useEffect(() => {
         getSecondPayment(requestId).then(response => setSecondPayment(response))
