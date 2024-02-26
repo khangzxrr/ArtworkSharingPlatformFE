dc214fba47067ffeda5bae487c01ce28cc598e4a
khangzxrr <khangzxrr@gmail.com>
2024-02-26 10:22:43 +0700

    add status to overcome error

--- src/hooks/getRequestHook.jsx
+++ src/hooks/getRequestHook.jsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from "react"
 import { userGetRequest, creatorGetRequest } from "services/requestService"
 import { isContainCreatorRole, isContainUserRole } from "stores/authenticationStore"
 
-export const useGetRequestById = (requestId, pageState) => {
-    const [request, setRequest] = useState({ user: { login: '' } })
+export const useGetRequestById = (requestId, pageState = 0) => {
+    const [request, setRequest] = useState({ status: '', user: { login: '' } })
 
     console.log('getting request')
 
