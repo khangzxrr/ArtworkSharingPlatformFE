b002af5dbbec6c52f7226d3a16a8c4a98f7ec599
khangzxrr <khangzxrr@gmail.com>
2024-02-26 10:21:24 +0700

    navigate after choose request bid

--- src/components/requestBidList/index.jsx
+++ src/components/requestBidList/index.jsx
@@ -1,18 +1,23 @@
 
 
-import { List, notification } from 'antd';
+import { Button, List, notification } from 'antd';
 import React from 'react';
 import { audienceChooseRequestBid } from '../../services/requestBidService';
 import { useAuthenticationStore } from '../../stores/authenticationStore';
+import { useNavigate } from 'react-router-dom';
 const Index = (props) => {
 
-  
+  const navigate = useNavigate()
   const account = useAuthenticationStore(state => state.account)
 
   function chooseRequestBid(requestBidId) {
     console.log(requestBidId)
     audienceChooseRequestBid(props.request.id, requestBidId)
-    .then(response => notification.info({ message: 'request bid',  description: 'choosed this request bid, you will be redirect soon...'}))
+    .then(response => {
+      notification.info({ message: 'request bid',  description: 'choosed this request bid, you will be redirect soon...'})
+
+      navigate(`/requests/${props.request.id}/progress`)
+    })
     .catch(err => {
       console.log(err)
       notification.error({ message: 'request bid', description: 'fail to choose this request bid'})
@@ -30,10 +35,10 @@ const Index = (props) => {
       renderItem={(item) => (
         <List.Item
         
-          actions={ props.request.user && props.request.user.login === account.login && props.request.status === 'ON_BIDING' ? [<a onClick={() => chooseRequestBid(item.id)}>choose this bid</a>] : []}
+          actions={ props.request.user && props.request.user.login === account.login && props.request.status === 'ON_BIDING' ? [<Button type='primary' onClick={() => chooseRequestBid(item.id)}>choose this bid</Button>] : []}
         >
           <List.Item.Meta
-            title={`price: ${item.price} duration: ${item.duration} hours (${item.status == 'SELECTED_BID' ? 'Choosed' : ''})`}
+            title={`price: ${item.price}$  - ${item.duration} hours ${item.status == 'SELECTED_BID' ? '(Choosed ⭐)' : ''}`}
             description={item.description}
           />
         </List.Item>
