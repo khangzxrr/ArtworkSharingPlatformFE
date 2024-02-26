8d80dde39e05fab3b4cce2a202b30f01e9babe6c
khangzxrr <khangzxrr@gmail.com>
2024-02-26 10:23:11 +0700

    seperate condition, add refreshPage

--- src/pages/requestPage/index.jsx
+++ src/pages/requestPage/index.jsx
@@ -1,22 +1,80 @@
-import React from "react";
-import { Button } from "antd";
-import { RequestBidList, RequestForm, RequestList } from "../../components";
-import { Link, useParams } from "react-router-dom";
+import React, { useState } from "react";
+import { Button, Col, Row } from "antd";
+import { RequestBidForm, RequestBidList, RequestForm } from "../../components";
+import { useNavigate, useParams } from "react-router-dom";
 import { useGetRequestById } from "hooks/getRequestHook";
 import { useGetRequestBidsByRequestId } from "hooks/requestBidHook";
-import { useAuthenticationStore } from "stores/authenticationStore";
+import styles from './index.module.css'
+import { isContainCreatorRole, isContainUserRole, useAuthenticationStore } from "stores/authenticationStore";
 
 const Index = () => {
+
+    const navigate = useNavigate()
+
+    const [pageState, setPageState] = useState(0)
+
+    const refreshPage = () => {
+        const newState = pageState + 1
+
+        setPageState(newState)
+    }
     const { requestId } = useParams()
 
-    
-    const request = useGetRequestById(requestId)
-    const requestBids = useGetRequestBidsByRequestId(requestId)
+    const request = useGetRequestById(requestId, pageState)
+    const requestBids = useGetRequestBidsByRequestId(requestId, pageState)
+
+    const account = useAuthenticationStore(state => state.account)
+
+
+    const showProgressButtonCondition = () => {
+
+
+        if (isContainUserRole()) {
+            return request.user && request.user.login === account.login && request.status !== 'ON_BIDING'
+        }
+
+
+        const selectedBid = requestBids.find(rb => rb.status === 'SELECTED_BID')
+
+        if (selectedBid === undefined) return false
+        
+        if (isContainCreatorRole()) {
+            return selectedBid.user.login === account.login && request.status !== 'ON_BIDING'
+        }
+    }
+
 
     return (
         <>
-            <RequestForm request={request} requestBids={requestBids} />
-            <RequestBidList request={request} requestBids={requestBids}  />
+            <Row className={styles.requestForm}>
+                <Col span={16} offset={3}>
+                    <RequestForm request={request} requestBids={requestBids} />
+                </Col>
+
+                {
+                    showProgressButtonCondition() &&
+                    <Col span={4}>
+                        <Button type="primary" onClick={() => navigate('progress')}>Go to progress page</Button>
+                    </Col>
+                }
+
+            </Row>
+            <Row>
+                <Col span={6} offset={3}>
+                    <RequestBidList request={request} requestBids={requestBids} />
+                </Col>
+                {(request.user.login !== account.login && isContainCreatorRole() && request.status === 'ON_BIDING') &&
+                    <Col span={6} offset={3}>
+                        <h2>Place a new request bid</h2>
+
+                        <RequestBidForm refreshPage={refreshPage} requestId={requestId} />
+
+                    </Col>
+                }
+
+            </Row>
+
+
         </>
 
     )
