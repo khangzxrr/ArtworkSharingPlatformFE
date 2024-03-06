d434caf1af929b290a6d04b27a7c1037a57cc65a
khangzxrr <khangzxrr@gmail.com>
2024-03-07 02:35:27 +0700

    add get and create artwork API

--- src/services/artworkService.jsx
+++ src/services/artworkService.jsx
@@ -6,8 +6,46 @@ export const audienceGetPublicArtworks = async () => {
     return { list: response.data, totalCount: response.headers['x-total-count'] }
 }
 
+export const audienceGetPrivateArtworks = async () => {
+    const response = await userAxios.get('/audience/artworks/mine')
+
+    return { list: response.data, totalCount: response.headers['x-total-count'] }
+}
+
 export const creatorGetPublicArtworks = async () => {
     const response = await userAxios.get('/creator/artworks')
 
     return { list: response.data, totalCount: response.headers['x-total-count'] }
+}
+
+export const creatorGetPrivateArtworks = async () => {
+    const response = await userAxios.get('/creator/artworks/mine')
+
+    return { list: response.data, totalCount: response.headers['x-total-count'] }
+}
+
+export const creatorCreateArtwork = async (categoryId, name, description, visibility, thumbnailUrl, otherAssetUrls) => {
+
+    const assets = [
+        {
+            thumbnail: true,
+            media: {
+                url: thumbnailUrl
+            }
+        },
+        ...otherAssetUrls.map(u => ({
+            thumbnail: false,
+            media: { url: u }
+        }))
+    ]
+
+    const response = await userAxios.post('/creator/artworks', {
+        name,
+        categoryId,
+        description,
+        assets,
+        visibility
+    })
+
+    return response.data
 }
