06c99aeb761b85b9d1d9b6d547d7626d1912116d
khangzxrr <khangzxrr@gmail.com>
2024-03-09 00:10:59 +0700

    add bothRole request methods

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,5 +1,36 @@
 import axios from "axios";
 import { BASE_URL } from "./constants";
+import { isContainCreatorRole, isContainUserRole } from "stores/authenticationStore";
 
-export const guestAxios = axios.create({ baseURL: BASE_URL})
-export const userAxios = axios.create({ baseURL: BASE_URL})
+export const guestAxios = axios.create({ baseURL: BASE_URL })
+export const userAxios = axios.create({ baseURL: BASE_URL })
+
+export const bothRolesPut = async (requestUrl, body) => {
+    if (isContainUserRole()) {
+        return userAxios.put(`/audience/${requestUrl}`, body)
+    }
+    else
+        if (isContainCreatorRole()) {
+            return userAxios.put(`/creator/${requestUrl}`, body)
+        }
+}
+
+export const bothRolesPost = async (requestUrl, body) => {
+    if (isContainUserRole()) {
+        return userAxios.post(`/audience/${requestUrl}`, body)
+    }
+    else
+        if (isContainCreatorRole()) {
+            return userAxios.post(`/creator/${requestUrl}`, body)
+        }
+}
+
+export const bothRolesGet = async (requestUrl) => {
+    if (isContainUserRole()) {
+        return userAxios.get(`/audience/${requestUrl}`)
+    }
+    else
+        if (isContainCreatorRole()) {
+            return userAxios.get(`/creator/${requestUrl}`)
+        }
+}
